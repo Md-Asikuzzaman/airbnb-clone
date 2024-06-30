eb9ec17d775e3bf6b3e600651ae90474f53643ef
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-06-30 11:38:51 +0600

    feat: added user menu component

--- src/app/components/navbar/Logo.tsx
+++ src/app/components/navbar/Logo.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Logo = () => {
   const router = useRouter();
 
   return (
     <Image
-      onClick={() => router.push('/')}
-      alt='Logo'
+      onClick={() => router.push("/")}
+      alt="Logo"
       height={100}
       width={100}
-      src='/images/logo.webp'
+      src="/images/logo.png"
     />
   );
 };
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,15 +1,17 @@
-import Container from '../Container';
-import Logo from './Logo';
-import Search from './Search';
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full bg-white z-10 shadow-sm'>
-      <div className='py-4 border-b-[1px]'>
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
         <Container>
-          <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
