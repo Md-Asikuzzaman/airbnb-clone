7447866c38d3a1c5e8e5c29e2dd9f4ecf266a6be
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-06-30 13:04:54 +0600

    feat: added button component

---
+++ src/app/components/modals/Modal.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import clsx from "clsx";
+import { NextPage } from "next";
+import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
+
+interface Props {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
+}
+
+const Modal: NextPage<Props> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel,
+}) => {
+  const [showModal, setShowModal] = useState<boolean | undefined>(isOpen);
+
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen]);
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    setShowModal(false);
+
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [disabled, onClose]);
+
+  const handleSunmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return;
+    }
+
+    secondaryAction();
+  }, [disabled, secondaryAction]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+      <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+        {/* MODAL */}
+        <div
+          className={clsx(
+            "translate duration-300 h-full",
+            showModal
+              ? "translate-y-0 opacity-100"
+              : "translate-y-full opacity-0"
+          )}
+        >
+          <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/* MODAL HEADER */}
+            <div className="relative flex items-center justify-center p-6 rounded-t border-b-[1px]">
+              <button
+                onClick={handleClose}
+                type="button"
+                className="
+                  p-1
+                  border-0
+                  hover:opacity-70
+                  transition
+                  absolute
+                  left-9
+                "
+              >
+                <IoMdClose />
+              </button>
+              <div className="text-lg font-semibold">{title}</div>
+            </div>
+            {/* MODAL BODY */}
+            <div className="relative p-6 flex-auto">{body}</div>
+            {/* MODAL FOOTER */}
+            <div className="flex flex-col gap-2 p-6">
+              <div className="flex flex-row items-center gap-4 w-full">
+                <Button icon={IoMdClose} label="Sign in" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
